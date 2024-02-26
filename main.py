9ad725d97053409b4b966e3c913d567d0cfcb888
Dipendra Nath <dipen.2052@gmail.com>
2024-02-26 17:36:44 -0500

    note engine added

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv, get_key
 import os
 import pandas as pd 
 from llama_index.core.query_engine import PandasQueryEngine
 from prompts import new_prompt, instruction_str 
-load_dotenv()
+from note_engine import note_engine
+from llama_index.core.tools import QueryEngineTool, ToolMetadata
+from llama_index.core.agent import ReActAgent
+from llama_index.llms import OpenAI
+os.environ["OPENAI_API_KEY"] = get_key(find_dotenv(), "OPENAI_API_KEY")
+
 
 
 population_path = os.path.join('data', 'population.csv')
@@ -13,4 +18,16 @@ population_df = pd.read_csv(population_path)
 population_query_engine = PandasQueryEngine(df=population_df, verbose=True, instruction_str=instruction_str)
 
 population_query_engine.update_prompts({"pandas_prompt": new_prompt})
-population_query_engine.query("What is the population of the United States in 2010?")
+
+
+population_query_engine.query("What is the population of canada")
+tools = [
+    note_engine,
+    QueryEngineTool(
+        query_engine=population_query_engine,
+        metadata=ToolMetadata(
+            name="population_query",
+            description="Query the population of a country"
+        )
+    )
+]
